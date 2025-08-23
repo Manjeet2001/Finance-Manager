699598d7433810add4f1e583741cdccc86e5ea46
Manjeet2001 <manjeetydv16@gmail.com>
2025-08-23 09:28:44 +0530

    SecurityConfig to handle favicon error

--- src/main/java/finance_manager/config/SecurityConfig.java
+++ src/main/java/finance_manager/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/favicon.ico").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(AbstractHttpConfigurer::disable)
