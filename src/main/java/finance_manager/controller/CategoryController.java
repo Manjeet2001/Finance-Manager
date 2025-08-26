18f5b7e726cf60ee9af74efadbc1becfa27ecac2
Manjeet2001 <manjeetydv16@gmail.com>
2025-08-26 23:57:26 +0530

    implemented secured endpoints for category service using SecurityUtil instead of sessions

--- src/main/java/finance_manager/controller/CategoryController.java
+++ src/main/java/finance_manager/controller/CategoryController.java
@@ -2,8 +2,11 @@ package finance_manager.controller;
 
 import finance_manager.dto.CategoryRequest;
 import finance_manager.entity.Category;
+import finance_manager.exception.custom_exception.UnauthorizedException;
+import finance_manager.security.SecurityUtils;
 import finance_manager.service.CategoryService;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.parameters.P;
@@ -13,23 +16,29 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
+@RequiredArgsConstructor
 public class CategoryController {
-    @Autowired
-    private CategoryService categoryService;
+    private final CategoryService categoryService;
 
     @PostMapping
-    public ResponseEntity<Category> createCustomCategory(@RequestBody CategoryRequest request, HttpSession session) {
-        return ResponseEntity.ok(categoryService.createCustomCategory(request, session));
+    public ResponseEntity<Category> createCustomCategory(@RequestBody CategoryRequest request) {
+        String username = SecurityUtils.getCurrentUsername();
+        if(username == null) throw new UnauthorizedException("Please login first");
+        return ResponseEntity.ok(categoryService.createCustomCategory(request, username));
     }
 
     @GetMapping
-    public ResponseEntity<List<Category>> getUserCategories(HttpSession session) {
-        return ResponseEntity.ok(categoryService.getUserCategories(session));
+    public ResponseEntity<List<Category>> getUserCategories() {
+        String username = SecurityUtils.getCurrentUsername();
+        if(username == null) throw new UnauthorizedException("Please login first");
+        return ResponseEntity.ok(categoryService.getUserCategories(username));
     }
 
     @DeleteMapping("/{name}")
-    public ResponseEntity<String> deleteCustomCategory(@PathVariable String name, HttpSession session) {
-        categoryService.deleteCustomCategory(name, session);
+    public ResponseEntity<String> deleteCustomCategory(@PathVariable String name) {
+        String username = SecurityUtils.getCurrentUsername();
+        if(username == null) throw new UnauthorizedException("Please login first");
+        categoryService.deleteCustomCategory(name, username);
         return ResponseEntity.ok("Category Deleted with name " + name);
     }
 
--- src/main/java/finance_manager/service/CategoryService.java
+++ src/main/java/finance_manager/service/CategoryService.java
@@ -2,28 +2,30 @@ package finance_manager.service;
 
 import finance_manager.dto.CategoryRequest;
 import finance_manager.entity.Category;
+import finance_manager.entity.User;
 import finance_manager.exception.custom_exception.ConflictException;
 import finance_manager.exception.custom_exception.ForbiddenException;
+import finance_manager.exception.custom_exception.ResourceNotFoundException;
 import finance_manager.exception.custom_exception.UnauthorizedException;
 import finance_manager.repository.CategoryRepository;
 import finance_manager.repository.UserRepository;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CategoryService {
-    @Autowired
-    private CategoryRepository categoryRepository;
 
-    @Autowired
-    private UserRepository userRepository;
+    private final CategoryRepository categoryRepository;
+    private final UserRepository userRepository;
 
-    public Category createCustomCategory(CategoryRequest request, HttpSession session) {
-        Long userId = getUserId(session);
-
+    public Category createCustomCategory(CategoryRequest request, String username) {
+        User user = getUserByUserName(username);
+        Long userId = user.getId();
         if(categoryRepository.existsByNameAndUserId(request.getName(), userId))
             throw new ConflictException("Category already exists");
 
@@ -36,23 +38,24 @@ public class CategoryService {
         return categoryRepository.save(category);
     }
 
-    public List<Category> getUserCategories(HttpSession session) {
-        Long userId = getUserId(session);
+    public List<Category> getUserCategories(String username) {
+        User user = getUserByUserName(username);
+        Long userId = user.getId();
         return categoryRepository.findByUserIdOrIsCustomFalse(userId);
     }
 
-    public void deleteCustomCategory(String name, HttpSession session) {
-        Long userId = getUserId(session);
+    public void deleteCustomCategory(String name, String username) {
+        User user = getUserByUserName(username);
+        Long userId = user.getId();
         Category category = categoryRepository.findByNameAndUserIdOrIsCustom(name, userId)
-                .orElseThrow(()-> new UnauthorizedException("category not found"));
+                .orElseThrow(()-> new ResourceNotFoundException("category not found"));
 
         if(!category.isCustom()) throw new ForbiddenException("cannot delete default Category");
         categoryRepository.delete(category);
     }
 
-    private Long getUserId(HttpSession session) {
-        Long userId = (Long) session.getAttribute("userId");
-        if (userId == null) throw new UnauthorizedException("Access denied");
-        return userId;
+    private User getUserByUserName(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(()->new ResourceNotFoundException("User not found for username: " + username));
     }
 }
