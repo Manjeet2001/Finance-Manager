be8646eed33055564c6378cb6532ee46258ad5d8
Manjeet2001 <manjeetydv16@gmail.com>
2025-08-28 17:11:40 +0530

    used swagger UI for minimalistic frontend

---
+++ src/main/java/finance_manager/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package finance_manager.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Finance-Manager APIs")
+                        .version("1.0")
+                        .description("This is the API documentation for my Spring Boot application"));
+    }
+}
